957684b80aa4ef123da05df9f00d4ea05a6b0f4f
Joshua Alphonse <joshalphonse@gmail.com>
2019-01-22 11:01:05 -0500

    User Controller, and html done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :categories, through: :posts
-
+  validates :name, presence: true
+  validates :password, presence: true,length: {in:6..16}
+  validates :username, presence:true ,uniqueness:true
 end
