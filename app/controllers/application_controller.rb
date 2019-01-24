b395b246e0407056033b8703875c3823a48860ca
Joshua Alphonse <joshalphonse@gmail.com>
2019-01-24 11:27:55 -0500

    Changed Layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authorized
+  before_action :current_dude, only: [:index,:show,:create,:new]
 
   helper_method :logged_in?
 
@@ -17,5 +18,10 @@ end
       if session[:user_id]
        @current_user = User.find(session[:user_id])
      end
+  end
+
+
+  def current_dude
+      @user= current_user
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,16 +6,19 @@ class PostsController < ApplicationController
   end
 
   def show
+
     @post = Post.find(params[:id])
+    @user=current_user
+
     @post.punch(request)
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
-  def create 
-    @post = Post.new(post_params)
+  def create
+    @post = current_user.posts.build(post_params)
     if @post.valid?
       @post.save
       redirect_to post_path(@post)
@@ -23,6 +26,19 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+
+  # @user = User.find_by(username: params[:username])
+  #
+  # if @user && @user.authenticate(params[:password])
+  #   session[:user_id] = @user.id
+  #   redirect_to @user
+
+
+
+
+
+
 
   def edit
     @post = Post.find(params[:id])
